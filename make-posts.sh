33f8aa8a36b8dbbe325f8dc948937cd3a4771725
tobbelobb <tobben@fastmail.fm>
2018-08-28 21:19:53 +0200

    Creates html files from xml locally

---
+++ make-posts.sh
@@ -0,0 +1,5 @@
+saxon-xslt 2018.xml blogtemplate.xsl > 2018.html
+saxon-xslt 2017.xml blogtemplate.xsl > 2017.html
+saxon-xslt 2016.xml blogtemplate.xsl > 2016.html
+saxon-xslt 2015.xml blogtemplate.xsl > 2015.html
+saxon-xslt 2014.xml blogtemplate.xsl > 2014.html
