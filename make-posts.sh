377f48559fb869e2bccafcf3250038133b8c5330
Torbjørn Ludvigsen <tobben@fastmail.fm>
2020-08-30 21:57:07 +0200

    Changes structure

--- make-posts.sh
+++ make-posts.sh
@@ -1,6 +1,6 @@
-saxon-xslt 2020.xml blogtemplate.xsl > 2020.html
-saxon-xslt 2018.xml blogtemplate.xsl > 2018.html
-saxon-xslt 2017.xml blogtemplate.xsl > 2017.html
-saxon-xslt 2016.xml blogtemplate.xsl > 2016.html
-saxon-xslt 2015.xml blogtemplate.xsl > 2015.html
-saxon-xslt 2014.xml blogtemplate.xsl > 2014.html
+saxon-xslt 2020.xml blogtemplate.xsl > index.html
+saxon-xslt 2018/2018.xml blogtemplate.xsl > 2018/index.html
+saxon-xslt 2017/2017.xml blogtemplate.xsl > 2017/index.html
+saxon-xslt 2016/2016.xml blogtemplate.xsl > 2016/index.html
+saxon-xslt 2015/2015.xml blogtemplate.xsl > 2015/index.html
+saxon-xslt 2014/2014.xml blogtemplate.xsl > 2014/index.html
