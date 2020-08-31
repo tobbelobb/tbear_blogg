c14c724d9728c7e1c215821dc30491c715a50ce9
Torbjørn Ludvigsen <tobben@fastmail.fm>
2020-08-31 18:27:22 +0200

    Makes a copy of 2020 the front page

--- make-posts.sh
+++ make-posts.sh
@@ -4,3 +4,8 @@ saxon-xslt 2017/2017.xml blogtemplate.xsl > 2017/index.html
 saxon-xslt 2016/2016.xml blogtemplate.xsl > 2016/index.html
 saxon-xslt 2015/2015.xml blogtemplate.xsl > 2015/index.html
 saxon-xslt 2014/2014.xml blogtemplate.xsl > 2014/index.html
+
+# Let the main page be a copy of the 2020 page for now
+cp 2020/index.html index.html
+sed -i 's/="\.\/bilder/="bilder/g' index.html
+sed -i 's/="bilder/="2020\/bilder/g' index.html
