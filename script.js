bdf4295506abdd03060c5887814de0761b22a23e
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-05-08 10:51:18 +0200

    Fixes link hover colors

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+function handleDottedLineHover() {
+  const dottedLine1 = document.getElementById('dotted-line-1');
+  const textBelowLine1 = document.getElementById('text-below-line-1');
+  const dottedLine2 = document.getElementById('dotted-line-2');
+  const textBelowLine2 = document.getElementById('text-below-line-2');
+
+  dottedLine1.addEventListener('mouseenter', () => {
+    textBelowLine1.classList.add('text-below-line-hover');
+  });
+  dottedLine2.addEventListener('mouseenter', () => {
+    textBelowLine2.classList.add('text-below-line-hover');
+  });
+
+  dottedLine1.addEventListener('mouseleave', () => {
+    textBelowLine1.classList.remove('text-below-line-hover');
+  });
+  dottedLine2.addEventListener('mouseleave', () => {
+    textBelowLine2.classList.remove('text-below-line-hover');
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   updateDottedLine('background-1', 'dotted-line-1', 'text-below-line-1', 'top-left-1');
   updateDottedLine('background-2', 'dotted-line-2', 'text-below-line-2', '');
@@ -12,6 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   handleDropdownDisplay();
+  handleDottedLineHover();
 });
 
 window.addEventListener('resize', () => {
