fc96d8fcbb866dbbc30295a232d7ca14538612be
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-05-09 17:52:26 +0200

    Put slide animation back for touchscreens

--- Heart_of_the_Ocean/hoto_script.js
+++ Heart_of_the_Ocean/hoto_script.js
@@ -1,11 +1,19 @@
+function enableSlideAnimationIfTouchscreen() {
+  const carousel = document.getElementById("carouselExampleControls");
+  const hasTouchscreen = window.matchMedia("(pointer: coarse)").matches;
+
+  if (hasTouchscreen) {
+    carousel.classList.add("slide");
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   handleDropdownDisplay();
-
 
   document.getElementById("buyButton").addEventListener("click", function () {
     Cart.addToCart("Heart of the Ocean", 500.00);
     updateCartDisplay();
   });
   updateCartDisplay();
+  enableSlideAnimationIfTouchscreen();
 });
