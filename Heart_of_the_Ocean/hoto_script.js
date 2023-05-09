8bb836ec47272702e7b8d9530a1a96819ec07d99
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-05-09 16:58:13 +0200

    Switches from viewerjs to photoswipe

--- Heart_of_the_Ocean/hoto_script.js
+++ Heart_of_the_Ocean/hoto_script.js
@@ -1,37 +1,7 @@
+
 document.addEventListener('DOMContentLoaded', () => {
   handleDropdownDisplay();
-  const carouselImages = document.getElementById('carousel-images');
-    const viewer = new Viewer(carouselImages, {
-      inline: false,
-      navbar: 4,
-      tooltip: false,
-      scalable: true,
-      fullscreen: true,
-      button: true,
-      transition: false,
-      toolbar: {
-          zoomIn: {
-            show: 1,
-            size: 'large',
-          },
-          zoomOut: {
-            show: 1,
-            size: 'large',
-          },
-          reset: {
-            show: 1,
-            size: 'large',
-          },
-          prev: {
-            show: 1,
-            size: 'large',
-          },
-          next: {
-            show: 1,
-            size: 'large',
-          },
-        },
-    });
+
 
   document.getElementById("buyButton").addEventListener("click", function () {
     Cart.addToCart("Heart of the Ocean", 500.00);
