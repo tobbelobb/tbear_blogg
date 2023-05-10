a144b40564fbf031953d78c31e6c8d7dba1a5b1f
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-05-10 14:24:12 +0200

    Shows thumbnails below carousel. Fading transition on mobile.

--- Heart_of_the_Ocean/hoto_script.js
+++ Heart_of_the_Ocean/hoto_script.js
@@ -5,6 +5,35 @@ function enableSlideAnimationIfTouchscreen() {
   if (hasTouchscreen) {
     carousel.classList.add("slide");
   }
+
+  let isTransitioning = false;
+  var thumbnailElems = document.querySelectorAll('.carousel-thumbnails img');
+  thumbnailElems.forEach(function (thumbnailElem) {
+      thumbnailElem.addEventListener('click', function () {
+          if (isTransitioning && hasTouchscreen) {
+            return;
+          }
+          if (hasTouchscreen) {
+            isTransitioning = true;
+            document.querySelector('#carouselExampleControls').classList.add('carousel-fade');
+          }
+
+          var goToSlide = this.getAttribute('data-slide-to');
+          var carousel = document.getElementById('carouselExampleControls');
+          var bsCarousel = new bootstrap.Carousel(carousel);
+          bsCarousel.to(parseInt(goToSlide));
+
+
+          if (hasTouchscreen) {
+            setTimeout(() => {
+              document.querySelector('#carouselExampleControls').classList.remove('carousel-fade');
+              isTransitioning = false;
+            }, 600);
+          }
+      });
+  });
+
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
