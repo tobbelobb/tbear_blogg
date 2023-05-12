d66c989bd6056bfa6f2dd0fc7681ad1eed3bdd08
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-05-12 17:24:07 +0200

    Nav css color styling

--- Heart_of_the_Ocean/hoto_script.js
+++ Heart_of_the_Ocean/hoto_script.js
@@ -39,19 +39,6 @@ function enableSlideAnimationIfTouchscreen() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  // If background brightness is high, make navbar darker to increase text contrast
-  let bgColor = window.getComputedStyle(document.body).backgroundColor;
-  let colorMatch = bgColor.match(/\d+/g);
-  let r = parseInt(colorMatch[0]);
-  let g = parseInt(colorMatch[1]);
-  let b = parseInt(colorMatch[2]);
-  let brightness = Math.round((r * 299 + g * 587 + b * 114) / 1000);
-  let element = document.querySelector('.transparent-nav');
-  console.log(brightness);
-  if (brightness > 128) {
-    element.style.backgroundColor = "rgba(0,0,0,0.85)";
-  }
-
   const link = document.createElement('link');
   link.rel = 'stylesheet';
   link.href = '../photoswipe/dist/photoswipe.css';
