e50856a8a349f3cfd80aeeac566dc0b1c142acea
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-05-15 02:13:09 +0200

    New front page. Shop moved to /shop

---
+++ frontpage_script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("form");
+
+form.addEventListener('submit', e => {
+  if (!form.checkValidity()) {
+    e.preventDefault();
+  }
+  form.classList.add("was-validated");
+})
