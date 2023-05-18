3c7c0ea687e22adc43de2e15ab8d84fbefb6d0be
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-05-18 20:17:31 +0200

    Makes animated menu from right edge

--- frontpage_script.js
+++ frontpage_script.js
@@ -6,3 +6,24 @@ form.addEventListener('submit', e => {
   }
   form.classList.add("was-validated");
 })
+
+document.addEventListener('click', e => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) {
+    return;
+  }
+
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle('active');
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown) {
+      return;
+    }
+    dropdown.classList.remove('active');
+  });
+})
