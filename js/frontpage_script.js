e9c12340b026bcac2c2c8404ecae39f1a43406a6
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-05-23 17:46:31 +0200

    Swipes away menu

--- js/frontpage_script.js
+++ js/frontpage_script.js
@@ -28,10 +28,45 @@ document.addEventListener('click', e => {
   });
 })
 
-var myVideo = document.getElementById("video1");
+const myVideo = document.getElementById("video1");
 function playPause() {
   if (myVideo.paused)
     myVideo.play();
   else
     myVideo.pause();
+}
+
+// Swipe away the menu
+let menuSwipeStartX = 0;
+let menuSwipeEndX = 0;
+let menuSwipeStartY = 0;
+let menuSwipeEndY = 0;
+if (isTouchDevice()) {
+  const menuSwipeArea = document.body;
+  menuSwipeArea.addEventListener("touchstart", e => {
+    menuSwipeStartX = e.changedTouches[0].screenX;
+    menuSwipeStartY = e.changedTouches[0].screenY;
+  }, false);
+  menuSwipeArea.addEventListener("touchend", e => {
+    menuSwipeEndX = e.changedTouches[0].screenX;
+    menuSwipeEndY = e.changedTouches[0].screenY;
+    menuGesture();
+  }, false);
+  menuSwipeArea.addEventListener("touchcancel", e => {
+    menuSwipeEndX = e.changedTouches[0].screenX;
+    menuSwipeEndY = e.changedTouches[0].screenY;
+    menuGesture();
+  }, false);
+  function menuGesture() {
+    if (menuSwipeEndX > (menuSwipeStartX + 10) &&
+      Math.abs(menuSwipeStartX - menuSwipeEndX) > Math.abs(menuSwipeStartY - menuSwipeEndY)) {
+      document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        dropdown.classList.remove('active');
+      })
+    }
+    menuSwipeStartX = 0;
+    menuSwipeEndX = 0;
+    menuSwipeStartY = 0;
+    menuSwipeEndY = 0;
+  }
 }
