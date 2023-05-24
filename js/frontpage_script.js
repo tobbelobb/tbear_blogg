8bd2b54160cbcc5912b0d17935f63e27e743f07c
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-05-24 19:32:14 +0200

    Adds shopping cart to front page

--- js/frontpage_script.js
+++ js/frontpage_script.js
@@ -75,19 +75,34 @@ if (isTouchDevice()) {
   }
 }
 
-vid = document.getElementById("video1");
-vid.addEventListener("pause", () => {
-  console.log("Paused");
-  const pl1 = document.getElementById("video1play");
-  const pl2 = document.getElementById("video1play2");
-  pl1.style.setProperty("visibility", "visible");
-  pl2.style.setProperty("visibility", "visible");
+
+document.addEventListener("DOMContentLoaded", () => {
+  vid = document.getElementById("video1");
+  vid.addEventListener("pause", () => {
+    const pl1 = document.getElementById("video1play");
+    const pl2 = document.getElementById("video1play2");
+    pl1.style.setProperty("visibility", "visible");
+    pl2.style.setProperty("visibility", "visible");
+  });
+
+  vid.addEventListener("play", () => {
+    const pl1 = document.getElementById("video1play");
+    const pl2 = document.getElementById("video1play2");
+    pl1.style.setProperty("visibility", "hidden");
+    pl2.style.setProperty("visibility", "hidden");
+  });
+
+  initiateCartDisplay();
 });
 
-vid.addEventListener("play", () => {
-  console.log("Played");
-  const pl1 = document.getElementById("video1play");
-  const pl2 = document.getElementById("video1play2");
-  pl1.style.setProperty("visibility", "hidden");
-  pl2.style.setProperty("visibility", "hidden");
+document.addEventListener("snipcart.ready", () => {
+  Snipcart.events.on("item.removed", (parsedCartItem) => {
+    updateCartDisplay();
+  });
+  Snipcart.events.on("item.added", (parsedCartItem) => {
+    updateCartDisplay();
+  });
+  Snipcart.events.on("cart.reset", (parsedCartItem) => {
+    updateCartDisplay();
+  });
 });
