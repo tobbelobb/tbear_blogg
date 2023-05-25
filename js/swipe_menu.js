7505e8beb97d97a2c947b873a2b9d4de651cc19a
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-05-25 13:50:31 +0200

    Experimental: swipe away meny by scrolling

--- js/swipe_menu.js
+++ js/swipe_menu.js
@@ -1,3 +1,67 @@
+/*!
+ * Run a callback after the user scrolls, calculating the distance and direction scrolled
+ * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Function} callback The callback function to run
+ * @param  {Integer}  refresh  How long to wait between scroll events [optional]
+ */
+function scrollDistance (callback, refresh = 100) {
+
+	// Make sure a valid callback was provided
+	if (!callback || typeof callback !== 'function') return;
+
+	// Variables
+	let isScrolling, start, end, distance;
+
+	// Listen for scroll events
+	window.addEventListener('scroll', function (event) {
+
+		// Set starting position
+		if (!start) {
+			start = window.pageYOffset;
+		}
+
+		// Clear our timeout throughout the scroll
+		window.clearTimeout(isScrolling);
+
+		// Set a timeout to run after scrolling ends
+		isScrolling = setTimeout(function() {
+
+			// Calculate distance
+			end = window.pageYOffset;
+			distance = end - start;
+
+			// Run the callback
+			callback(distance, start, end);
+
+			// Reset calculations
+			start = null;
+			end = null;
+			distance = null;
+
+		}, refresh);
+
+	}, false);
+
+}
+
+let acc_distance = 0;
+let scroll_updates = 0;
+scrollDistance(function (distance) {
+  if (document.querySelectorAll("[data-dropdown].active").length !== 0) {
+    acc_distance += distance;
+    scroll_updates += 1;
+    if (acc_distance > 1000 || acc_distance < -2000 || scroll_updates >= 3) {
+      document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        dropdown.classList.remove('active');
+      });
+      acc_distance = 0;
+      scroll_updates = 0;
+    }
+  }
+});
+
+
+
 document.addEventListener('click', e => {
   const isDropdownToggle = e.target.matches("[data-dropdown-toggle]");
 
