536ccf7637ccd24e36c1d723708383ddfffbaf80
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-08-02 19:56:21 +0200

    First sketch

--- blog/make-posts.sh
+++ blog/make-posts.sh
@@ -1,4 +1,5 @@
 saxon-xslt hangprinter-is-prior-art/hangprinter-is-prior-art.xml blogtemplate.xsl > hangprinter-is-prior-art/index.html
+saxon-xslt 2023/2023.xml blogtemplate.xsl > 2023/index.html
 saxon-xslt 2022/2022.xml blogtemplate.xsl > 2022/index.html
 saxon-xslt 2021/2021.xml blogtemplate.xsl > 2021/index.html
 saxon-xslt 2020/2020.xml blogtemplate.xsl > 2020/index.html
@@ -11,9 +12,9 @@ saxon-xslt 2014/2014.xml blogtemplate.xsl > 2014/index.html
 saxon-xslt super-secret/super-secret.xml blogtemplate.xsl > super-secret/index.html
 saxon-xslt rust/rust.xml blogtemplate.xsl > rust/index.html
 
-# Let the main page be a copy of the 2022 page for now
-cp 2022/index.html index.html
-# The new index should point to itself without the 2022 part
-sed -i 's/<meta property="og:url" content="https:\/\/torbjornludvigsen.com\/blog\/2022/<meta property="og:url" content="https:\/\/torbjornludvigsen.com\/blog/g' index.html
-# Remove the "this is 2022" part for front page blog version
-sed -i 's/Reprap Blog 2022//g' index.html
+# Let the main page be a copy of the 2023 page for now
+cp 2023/index.html index.html
+# The new index should point to itself without the 2023 part
+sed -i 's/<meta property="og:url" content="https:\/\/torbjornludvigsen.com\/blog\/2023/<meta property="og:url" content="https:\/\/torbjornludvigsen.com\/blog/g' index.html
+# Remove the "this is 2023" part for front page blog version
+sed -i 's/Reprap Blog 2023//g' index.html
