6ef8bc58e3f17182cee7d36d2cf8793d90e27c11
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-08-28 23:37:47 +0200

    Brings main cup pics up from 1500 to 3000 px

--- shop/Cups/media/gallery/convert_all.sh
+++ shop/Cups/media/gallery/convert_all.sh
@@ -1,44 +1,44 @@
-convert -resize 500 Cup1_back.JPG Cup1_back_small.JPG
-convert -resize 500 Cup2_back.JPG Cup2_back_small.JPG
-convert -resize 500 Cup3_back.JPG Cup3_back_small.JPG
-convert -resize 500 Cup4_back.JPG Cup4_back_small.JPG
-convert -resize 500 Cup5_back.JPG Cup5_back_small.JPG
-convert -resize 500 Cup6_back.JPG Cup6_back_small.JPG
-convert -resize 500 Cup7_back.JPG Cup7_back_small.JPG
-convert -resize 500 Cup8_back.JPG Cup8_back_small.JPG
-convert -resize 500 Cup9_back.JPG Cup9_back_small.JPG
-convert -resize 500 Cup10_back.JPG Cup10_back_small.JPG
-convert -resize 500 Cup11_back.JPG Cup11_back_small.JPG
-convert -resize 500 Cup1_front.JPG Cup1_front_small.JPG
-convert -resize 500 Cup2_front.JPG Cup2_front_small.JPG
-convert -resize 500 Cup3_front.JPG Cup3_front_small.JPG
-convert -resize 500 Cup4_front.JPG Cup4_front_small.JPG
-convert -resize 500 Cup5_front.JPG Cup5_front_small.JPG
-convert -resize 500 Cup6_front.JPG Cup6_front_small.JPG
-convert -resize 500 Cup7_front.JPG Cup7_front_small.JPG
-convert -resize 500 Cup8_front.JPG Cup8_front_small.JPG
-convert -resize 500 Cup9_front.JPG Cup9_front_small.JPG
-convert -resize 500 Cup10_front.JPG Cup10_front_small.JPG
-convert -resize 500 Cup11_front.JPG Cup11_front_small.JPG
-avifenc Cup1_back_small.JPG -o Cup1_back_small.avif
-avifenc Cup2_back_small.JPG -o Cup2_back_small.avif
-avifenc Cup3_back_small.JPG -o Cup3_back_small.avif
-avifenc Cup4_back_small.JPG -o Cup4_back_small.avif
-avifenc Cup5_back_small.JPG -o Cup5_back_small.avif
-avifenc Cup6_back_small.JPG -o Cup6_back_small.avif
-avifenc Cup7_back_small.JPG -o Cup7_back_small.avif
-avifenc Cup8_back_small.JPG -o Cup8_back_small.avif
-avifenc Cup9_back_small.JPG -o Cup9_back_small.avif
-avifenc Cup10_back_small.JPG -o Cup10_back_small.avif
-avifenc Cup11_back_small.JPG -o Cup11_back_small.avif
-avifenc Cup1_front_small.JPG -o Cup1_front_small.avif
-avifenc Cup2_front_small.JPG -o Cup2_front_small.avif
-avifenc Cup3_front_small.JPG -o Cup3_front_small.avif
-avifenc Cup4_front_small.JPG -o Cup4_front_small.avif
-avifenc Cup5_front_small.JPG -o Cup5_front_small.avif
-avifenc Cup6_front_small.JPG -o Cup6_front_small.avif
-avifenc Cup7_front_small.JPG -o Cup7_front_small.avif
-avifenc Cup8_front_small.JPG -o Cup8_front_small.avif
-avifenc Cup9_front_small.JPG -o Cup9_front_small.avif
-avifenc Cup10_front_small.JPG -o Cup10_front_small.avif
-avifenc Cup11_front_small.JPG -o Cup11_front_small.avif
+convert -resize 3000 Cup1_back.JPG Cup1_back.JPG
+convert -resize 3000 Cup2_back.JPG Cup2_back.JPG
+convert -resize 3000 Cup3_back.JPG Cup3_back.JPG
+convert -resize 3000 Cup4_back.JPG Cup4_back.JPG
+convert -resize 3000 Cup5_back.JPG Cup5_back.JPG
+convert -resize 3000 Cup6_back.JPG Cup6_back.JPG
+convert -resize 3000 Cup7_back.JPG Cup7_back.JPG
+convert -resize 3000 Cup8_back.JPG Cup8_back.JPG
+convert -resize 3000 Cup9_back.JPG Cup9_back.JPG
+convert -resize 3000 Cup10_back.JPG Cup10_back.JPG
+convert -resize 3000 Cup11_back.JPG Cup11_back.JPG
+convert -resize 3000 Cup1_front.JPG Cup1_front.JPG
+convert -resize 3000 Cup2_front.JPG Cup2_front.JPG
+convert -resize 3000 Cup3_front.JPG Cup3_front.JPG
+convert -resize 3000 Cup4_front.JPG Cup4_front.JPG
+convert -resize 3000 Cup5_front.JPG Cup5_front.JPG
+convert -resize 3000 Cup6_front.JPG Cup6_front.JPG
+convert -resize 3000 Cup7_front.JPG Cup7_front.JPG
+convert -resize 3000 Cup8_front.JPG Cup8_front.JPG
+convert -resize 3000 Cup9_front.JPG Cup9_front.JPG
+convert -resize 3000 Cup10_front.JPG Cup10_front.JPG
+convert -resize 3000 Cup11_front.JPG Cup11_front.JPG
+#avifenc Cup1_back_small.JPG -o Cup1_back_small.avif
+#avifenc Cup2_back_small.JPG -o Cup2_back_small.avif
+#avifenc Cup3_back_small.JPG -o Cup3_back_small.avif
+#avifenc Cup4_back_small.JPG -o Cup4_back_small.avif
+#avifenc Cup5_back_small.JPG -o Cup5_back_small.avif
+#avifenc Cup6_back_small.JPG -o Cup6_back_small.avif
+#avifenc Cup7_back_small.JPG -o Cup7_back_small.avif
+#avifenc Cup8_back_small.JPG -o Cup8_back_small.avif
+#avifenc Cup9_back_small.JPG -o Cup9_back_small.avif
+#avifenc Cup10_back_small.JPG -o Cup10_back_small.avif
+#avifenc Cup11_back_small.JPG -o Cup11_back_small.avif
+#avifenc Cup1_front_small.JPG -o Cup1_front_small.avif
+#avifenc Cup2_front_small.JPG -o Cup2_front_small.avif
+#avifenc Cup3_front_small.JPG -o Cup3_front_small.avif
+#avifenc Cup4_front_small.JPG -o Cup4_front_small.avif
+#avifenc Cup5_front_small.JPG -o Cup5_front_small.avif
+#avifenc Cup6_front_small.JPG -o Cup6_front_small.avif
+#avifenc Cup7_front_small.JPG -o Cup7_front_small.avif
+#avifenc Cup8_front_small.JPG -o Cup8_front_small.avif
+#avifenc Cup9_front_small.JPG -o Cup9_front_small.avif
+#avifenc Cup10_front_small.JPG -o Cup10_front_small.avif
+#avifenc Cup11_front_small.JPG -o Cup11_front_small.avif
