855bcbf5ea5f762483794cc6f7ae3a28549ff3fb
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-08-31 02:02:40 +0200

    Add cups to cart

--- js/cups_script.js
+++
@@ -1,8 +0,0 @@
-let addToCartButtons = document.getElementsByClassName("add-cup-to-cart");
-
-for (let i = 0; i < addToCartButtons.length; i++) {
-  addToCartButtons[i].addEventListener("click", function(event) {
-    alert("Div clicked: " + this.innerHTML);
-    event.stopPropagation();
-  });
-}
