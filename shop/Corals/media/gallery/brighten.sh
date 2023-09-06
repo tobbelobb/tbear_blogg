3edc752ed3d38b3354ca6df8983a4f1656562625
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-09-06 16:14:43 +0200

    Adds shop page for corals

---
+++ shop/Corals/media/gallery/brighten.sh
@@ -0,0 +1,15 @@
+#convert -brightness-contrast 20x0 Coral1_front.JPG Coral1_front.JPG
+#convert -brightness-contrast 20x0 Coral2_front.JPG Coral2_front.JPG
+#convert -brightness-contrast 20x0 Coral1_left.JPG Coral1_left.JPG
+#convert -brightness-contrast 20x0 Coral2_left.JPG Coral2_left.JPG
+#convert -brightness-contrast 20x0 Coral1_right.JPG Coral1_right.JPG
+#convert -brightness-contrast 20x0 Coral2_right.JPG Coral2_right.JPG
+#convert -brightness-contrast 20x0 Coral1_back.JPG Coral1_back.JPG
+#convert -brightness-contrast 20x0 Coral2_back.JPG Coral2_back.JPG
+#convert -brightness-contrast 20x0 Corals_2_and_1.JPG Corals_2_and_1.JPG
+
+convert -brightness-contrast 10x0 Coral3_front.JPG Coral3_front.JPG
+convert -brightness-contrast 10x0 Coral3_left.JPG Coral3_left.JPG
+convert -brightness-contrast 10x0 Coral3_right.JPG Coral3_right.JPG
+convert -brightness-contrast 10x0 Coral3_back.JPG Coral3_back.JPG
+
