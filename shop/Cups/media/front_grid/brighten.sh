3520a95317bd710a315812eefc5af8e44240a6f3
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-09-06 19:37:37 +0200

    Brightens shop front cup images

---
+++ shop/Cups/media/front_grid/brighten.sh
@@ -0,0 +1,44 @@
+convert -brightness-contrast 20x0 Cup1_front_small.JPG Cup1_front_small.JPG
+convert -brightness-contrast 20x0 Cup2_front_small.JPG Cup2_front_small.JPG
+convert -brightness-contrast 20x0 Cup3_front_small.JPG Cup3_front_small.JPG
+convert -brightness-contrast 20x0 Cup4_front_small.JPG Cup4_front_small.JPG
+convert -brightness-contrast 20x0 Cup5_front_small.JPG Cup5_front_small.JPG
+convert -brightness-contrast 20x0 Cup6_front_small.JPG Cup6_front_small.JPG
+convert -brightness-contrast 20x0 Cup7_front_small.JPG Cup7_front_small.JPG
+convert -brightness-contrast 20x0 Cup8_front_small.JPG Cup8_front_small.JPG
+convert -brightness-contrast 20x0 Cup9_front_small.JPG Cup9_front_small.JPG
+convert -brightness-contrast 20x0 Cup10_front_small.JPG Cup10_front_small.JPG
+convert -brightness-contrast 20x0 Cup11_front_small.JPG Cup11_front_small.JPG
+convert -brightness-contrast 20x0 Cup1_back_small.JPG Cup1_back_small.JPG
+convert -brightness-contrast 20x0 Cup2_back_small.JPG Cup2_back_small.JPG
+convert -brightness-contrast 20x0 Cup3_back_small.JPG Cup3_back_small.JPG
+convert -brightness-contrast 20x0 Cup4_back_small.JPG Cup4_back_small.JPG
+convert -brightness-contrast 20x0 Cup5_back_small.JPG Cup5_back_small.JPG
+convert -brightness-contrast 20x0 Cup6_back_small.JPG Cup6_back_small.JPG
+convert -brightness-contrast 20x0 Cup7_back_small.JPG Cup7_back_small.JPG
+convert -brightness-contrast 20x0 Cup8_back_small.JPG Cup8_back_small.JPG
+convert -brightness-contrast 20x0 Cup9_back_small.JPG Cup9_back_small.JPG
+convert -brightness-contrast 20x0 Cup10_back_small.JPG Cup10_back_small.JPG
+convert -brightness-contrast 20x0 Cup11_back_small.JPG Cup11_back_small.JPG
+avifenc Cup1_front_small.JPG -o Cup1_front_small.avif
+avifenc Cup2_front_small.JPG -o Cup2_front_small.avif
+avifenc Cup3_front_small.JPG -o Cup3_front_small.avif
+avifenc Cup4_front_small.JPG -o Cup4_front_small.avif
+avifenc Cup5_front_small.JPG -o Cup5_front_small.avif
+avifenc Cup6_front_small.JPG -o Cup6_front_small.avif
+avifenc Cup7_front_small.JPG -o Cup7_front_small.avif
+avifenc Cup8_front_small.JPG -o Cup8_front_small.avif
+avifenc Cup9_front_small.JPG -o Cup9_front_small.avif
+avifenc Cup10_front_small.JPG -o Cup10_front_small.avif
+avifenc Cup11_front_small.JPG -o Cup11_front_small.avif
+avifenc Cup1_back_small.JPG -o Cup1_back_small.avif
+avifenc Cup2_back_small.JPG -o Cup2_back_small.avif
+avifenc Cup3_back_small.JPG -o Cup3_back_small.avif
+avifenc Cup4_back_small.JPG -o Cup4_back_small.avif
+avifenc Cup5_back_small.JPG -o Cup5_back_small.avif
+avifenc Cup6_back_small.JPG -o Cup6_back_small.avif
+avifenc Cup7_back_small.JPG -o Cup7_back_small.avif
+avifenc Cup8_back_small.JPG -o Cup8_back_small.avif
+avifenc Cup9_back_small.JPG -o Cup9_back_small.avif
+avifenc Cup10_back_small.JPG -o Cup10_back_small.avif
+avifenc Cup11_back_small.JPG -o Cup11_back_small.avif
