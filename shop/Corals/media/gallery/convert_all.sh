42274d71d0991b805a83b734e03742694edd7a65
Torbjørn Ludvigsen <tobben@fastmail.fm>
2023-09-07 20:32:54 +0200

    Fixups Coral product page images

--- shop/Corals/media/gallery/convert_all.sh
+++ shop/Corals/media/gallery/convert_all.sh
@@ -1,30 +1,28 @@
-#convert -resize 2937 Coral1_front.JPG Coral1_front_medium.JPG
-#convert -resize 2937 Coral1_left.JPG  Coral1_left_medium.JPG
-#convert -resize 2937 Coral1_right.JPG Coral1_right_medium.JPG
-#convert -resize 2937 Coral1_back.JPG  Coral1_back_medium.JPG
-#convert -resize 2937 Coral2_front.JPG Coral2_front_medium.JPG
-#convert -resize 2937 Coral2_left.JPG  Coral2_left_medium.JPG
-#convert -resize 2937 Coral2_right.JPG Coral2_right_medium.JPG
-#convert -resize 2937 Coral2_back.JPG  Coral2_back_medium.JPG
-#convert -resize 3008 Coral3_front.JPG Coral3_front_medium.JPG
-#convert -resize 3008 Coral3_left.JPG  Coral3_left_medium.JPG
-#convert -resize 3008 Coral3_right.JPG Coral3_right_medium.JPG
-#convert -resize 3008 Coral3_back.JPG  Coral3_back_medium.JPG
-#convert -resize 3008 Corals_2_and_1.JPG  Corals_2_and_1_medium.JPG
-#
-#convert -resize 500 Coral1_front.JPG Coral1_front_small.JPG
-#convert -resize 500 Coral1_left.JPG  Coral1_left_small.JPG
-#convert -resize 500 Coral1_right.JPG Coral1_right_small.JPG
-#convert -resize 500 Coral1_back.JPG  Coral1_back_small.JPG
-#convert -resize 500 Coral2_front.JPG Coral2_front_small.JPG
-#convert -resize 500 Coral2_left.JPG  Coral2_left_small.JPG
-#convert -resize 500 Coral2_right.JPG Coral2_right_small.JPG
-#convert -resize 500 Coral2_back.JPG  Coral2_back_small.JPG
-#convert -resize 500 Coral3_front.JPG Coral3_front_small.JPG
-#convert -resize 500 Coral3_left.JPG  Coral3_left_small.JPG
-#convert -resize 500 Coral3_right.JPG Coral3_right_small.JPG
-#convert -resize 500 Coral3_back.JPG  Coral3_back_small.JPG
-#convert -resize 500 Corals_2_and_1.JPG  Corals_2_and_1_small.JPG
+convert -resize 2937 Coral1_front.JPG Coral1_front_medium.JPG
+convert -resize 2937 Coral1_left.JPG  Coral1_left_medium.JPG
+convert -resize 2937 Coral1_right.JPG Coral1_right_medium.JPG
+convert -resize 2937 Coral1_back.JPG  Coral1_back_medium.JPG
+convert -resize 2937 Coral2_front.JPG Coral2_front_medium.JPG
+convert -resize 2937 Coral2_left.JPG  Coral2_left_medium.JPG
+convert -resize 2937 Coral2_right.JPG Coral2_right_medium.JPG
+convert -resize 2937 Coral2_back.JPG  Coral2_back_medium.JPG
+convert -resize 3008 Coral3_front.JPG Coral3_front_medium.JPG
+convert -resize 3008 Coral3_left.JPG  Coral3_left_medium.JPG
+convert -resize 3008 Coral3_right.JPG Coral3_right_medium.JPG
+convert -resize 3008 Coral3_back.JPG  Coral3_back_medium.JPG
+
+convert -resize 500 Coral1_front.JPG Coral1_front_small.JPG
+convert -resize 500 Coral1_left.JPG  Coral1_left_small.JPG
+convert -resize 500 Coral1_right.JPG Coral1_right_small.JPG
+convert -resize 500 Coral1_back.JPG  Coral1_back_small.JPG
+convert -resize 500 Coral2_front.JPG Coral2_front_small.JPG
+convert -resize 500 Coral2_left.JPG  Coral2_left_small.JPG
+convert -resize 500 Coral2_right.JPG Coral2_right_small.JPG
+convert -resize 500 Coral2_back.JPG  Coral2_back_small.JPG
+convert -resize 500 Coral3_front.JPG Coral3_front_small.JPG
+convert -resize 500 Coral3_left.JPG  Coral3_left_small.JPG
+convert -resize 500 Coral3_right.JPG Coral3_right_small.JPG
+convert -resize 500 Coral3_back.JPG  Coral3_back_small.JPG
 
 avifenc Coral1_back_small.JPG  -o Coral1_back_small.avif
 avifenc Coral1_left_small.JPG  -o Coral1_left_small.avif
